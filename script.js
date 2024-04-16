8b4b7f658fdd3ab466054193495ad914d0cd9442
mohamed waleed <mdxx421@gmail.com>
2024-04-16 17:29:15 +0200

    V 1.9

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
+//buycard
+function submitcard(x,y,z){
+    allnumeric(x);
+    expire(y);
+    cardno(z);
+}
 function allnumeric(inputtxt)
 {
-    var numbers = /^[0-9]{2}[/]$/;
+    var numbers = /^[0-9]+$/;
     if(inputtxt.value.match(numbers))
     {
-        alert('Your Registration number has accepted....');
         return true;
     }
     else
@@ -18,7 +23,6 @@ function expire(inputtxt)
     var numbers = /^[0-9]{2}[/][0-9]{2}$/;
     if(inputtxt.value.match(numbers))
     {
-        alert('Your Registration number has accepted....');
         return true;
     }
     else
@@ -28,3 +32,18 @@ function expire(inputtxt)
         return false;
     }
 }
+function cardno(inputtxt)
+{
+    var numbers = /^[0-9]{4}[ ][0-9]{4}[ ][0-9]{4}[ ][0-9]{4}$/;
+    if(inputtxt.value.match(numbers))
+    {
+        return true;
+    }
+    else
+    {
+        alert('Please input numeric characters only');
+        inputtxt.focus();
+        return false;
+    }
+}
+//
